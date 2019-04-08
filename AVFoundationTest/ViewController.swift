04104f518b4ae4b9c27164e9b6a677f78886747d
Tasin Zarkoob <tasin.amin@mcdermott.com>
2019-04-08 17:27:46 +0530

    Initial Commit

---
+++ AVFoundationTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVFoundationTest
+//
+//  Created by Tasin Zarkoob on 08/04/2019.
+//  Copyright © 2019 Tasin Zarkoob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
